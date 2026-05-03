daf43327900e4bdf1f0677cc0e1918a24e175041
NoxS1d <148648283+NoxS1d-Dev@users.noreply.github.com>
2026-05-03 18:20:46 +0300

    build: refactor prepare.sh to use global vars, improve local build support and fix name generation

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+KERNEL_NAME="ElectroX"
+DATE=$(date +"%y.%m")
+
+DEFCONFIG="arch/arm64/configs/vendor/chime_defconfig"
+
+REPO_PACKAGING_TOOLS="https://github.com/NoxS1d-Dev/kernel-packaging.git"
+REPO_KERNELSU_PATCHES="https://github.com/NoxS1d-Dev/ksu-kernel-4.19.git"
+
 print_usage() {
     cat << EOF
 Usage: ./prepare.sh [KERNEL_VARIANT] [PATCH_VERSION] [FLAGS...]
@@ -82,30 +90,38 @@ echo "Preparing Build Environment."
 echo ""
 
 echo "Checkout Packaging Tools"
-git clone --depth=1 -b kernel-packaging https://github.com/NoxS1d-Dev/kernel-packaging.git toolchain/packaging
+rm -rf toolchain/packaging
+git clone --depth=1 -b kernel-packaging $REPO_PACKAGING_TOOLS toolchain/packaging
+echo ""
+
+echo "Removing Appended Configs in $DEFCONFIG"
+if grep -q "# KernelSU" "$DEFCONFIG"; then
+    echo "Appended configs found"
+    sed -i '/# KernelSU/,$d' "$DEFCONFIG"
+    sed -i '$d' "$DEFCONFIG"
+else
+    echo "Appended configs not found"
+fi
 echo ""
 
 if [ "$KSU" = true ]; then
     echo "Checkout KernelSU Patches"
-    git clone --depth=1 -b main https://github.com/NoxS1d-Dev/ksu-kernel-4.19.git toolchain/kernelsu
+    rm -rf toolchain/kernelsu
+    git clone --depth=1 -b main $REPO_KERNELSU_PATCHES toolchain/kernelsu
     echo ""
 
     echo "Apply KernelSU Patches"
-    cp ./toolchain/kernelsu/kernel_patches/add_ksu_in_kernel-4.19.patch ./
-    patch -p1 --verbose < add_ksu_in_kernel-4.19.patch
-    echo "KernelSU Patches Applied Successfully."
+    patch -p1 --verbose < toolchain/kernelsu/kernel_patches/add_ksu_in_kernel-4.19.patch
     echo ""
 
     if [ "$KSU_EXTRAS" = true ]; then
         echo "Apply KernelSU Extras Patches"
-        cp ./toolchain/kernelsu/kernel_patches/extras/add_ksu_extras_in_kernel-4.19.patch ./
-        patch -p1 --verbose < add_ksu_extras_in_kernel-4.19.patch
-        echo "KernelSU Extras Patches Applied Successfully."
+        patch -p1 --verbose < toolchain/kernelsu/kernel_patches/extras/add_ksu_extras_in_kernel-4.19.patch
         echo ""
     fi
 
     echo "Apply Configs"
-    cat <<EOF >> arch/arm64/configs/vendor/chime_defconfig
+    cat <<EOF >> "$DEFCONFIG"
 
 #
 # KernelSU
@@ -119,41 +135,40 @@ CONFIG_KSU_FEATURE_ADBROOT=y
 # CONFIG_KSU_DEBUG is not set
 # CONFIG_KSU_THRONE_TRACKER_ALWAYS_THREADED is not set
 CONFIG_KSU_LSM_SECURITY_HOOKS=y
+
 EOF
 
-    echo "Verifying chime_defconfig:"
-    tail -n 20 arch/arm64/configs/vendor/chime_defconfig
+    echo "Verifying $DEFCONFIG:"
+    tail -n 20 "$DEFCONFIG"
     echo ""
 
     echo "Setup KernelSU"
     curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -
     echo ""
-
-elif [ "$DEFAULT" = true ]; then
-    :
 fi
 
 echo "Generate Kernel Name"
-KERNEL_NAME=$(cat localversion | tr -d '\n')-$(date +"%y.%m").${PATCH_VERSION}-${KERNEL_VARIANT}
-echo "${KERNEL_NAME:1}"
+FULL_KERNEL_NAME=${KERNEL_NAME}-${DATE}.${PATCH_VERSION}-${KERNEL_VARIANT}
+echo "$FULL_KERNEL_NAME"
 echo ""
 
 if [ -n "$GITHUB_ENV" ]; then
-    echo "KERNEL_NAME=${KERNEL_NAME:1}" >> $GITHUB_ENV
+    echo "FULL_KERNEL_NAME=$FULL_KERNEL_NAME" >> $GITHUB_ENV
 fi
 
 echo "Modify localversion File"
-echo "$KERNEL_NAME" > localversion
+echo "-${FULL_KERNEL_NAME}" > localversion
 echo "Verifying localversion:"
 cat localversion
 echo ""
 
 echo "Configure AnyKernel3 version"
-sed -i "s/ElectroX Build.*/ElectroX Build : $(date +"%y.%m").${PATCH_VERSION}/g" toolchain/packaging/AnyKernel3/version
+sed -i "s/ElectroX Build.*/ElectroX Build : ${DATE}.${PATCH_VERSION}/g" toolchain/packaging/AnyKernel3/version
 sed -i "s/Variant.*/Variant        : ${VARIANT_DISPLAY}/g" toolchain/packaging/AnyKernel3/version
 echo ""
 
 echo "Stage Packaging Directory"
+rm -rf AnyKernel3
 cp -r toolchain/packaging/AnyKernel3 .
 echo ""
 
